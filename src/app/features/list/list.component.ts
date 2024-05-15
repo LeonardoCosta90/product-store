52e93c57e664fd76faeedaa9dac1e15f05b75e75
Leonardo <leonardo.everton@inatel.br>
2024-05-15 18:31:21 -0300

    refactor: improvments in list products

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
+  products: Product[] = [];
 
+  productsService = inject(ProductsService);
+
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
