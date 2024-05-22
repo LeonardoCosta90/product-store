98cf72d4c797f9d281e9115f3bd1b9df396f7a1c
Leonardo <leonardo.everton@inatel.br>
2024-05-22 14:05:53 -0300

    refactor: create resolvers especial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,14 @@ import {
 import { ListComponent } from './features/list/list.component';
 import { ProductsService } from './shared/services/products.service';
 import { inject } from '@angular/core';
+import { getProducts } from './shared/resolvers/get-products.resolver';
+import { getProduct } from './shared/resolvers/get-product.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     resolve: {
-      products: () => {
-        const productsService = inject(ProductsService);
-        return productsService.getAll();
-      },
+      products: getProducts,
     },
     component: ListComponent,
   },
@@ -28,10 +27,7 @@ export const routes: Routes = [
   {
     path: 'edit-product/:id',
     resolve: {
-      product: (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        const productsService = inject(ProductsService);
-        return productsService.get(route.paramMap.get('id') as string);
-      },
+      product: getProduct,
     },
     loadComponent: () =>
       import('./features/edit/edit.component').then((m) => m.EditComponent),
